858d1249f570148b6432ea4d5c0dde7cd03c8f00
FSB4coder <76252212+FSB4coder@users.noreply.github.com>
2024-06-02 16:00:05 +0300

    Update knopki.js

--- knopki.js
+++ knopki.js
@@ -1,15 +1,1 @@
-const box = document.querySelector('.box');
- 
-let xPos = 0;
-let yPos = 0;
- 
-function animateBox() {
-  xPos += 1;
-  yPos += 1;
- 
-  box.style.transform = `translate(${xPos}px, ${yPos}px)`;
- 
-  requestAnimationFrame(animateBox);
-}
- 
-animateBox();
+
